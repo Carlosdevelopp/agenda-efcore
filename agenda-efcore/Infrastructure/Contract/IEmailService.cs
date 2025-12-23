3661ccc8ad8947ee6a2af53d4a52fdd38d8aaaf6
Carlosdevelopp <carlosdevelopp@gmail.com>
2025-12-23 00:38:51 -0600

    Agrega reset password

---
+++ agenda-efcore/Infrastructure/Contract/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace Infrastructure.Contract;
+
+public interface IEmailService
+{
+    Task SendAsync(string to, string subject, string body);
+}
---
+++ agenda-efcore/MiAgendaEF/Models/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agenda.EFCore.Models.ViewModels;
+
+public class ResetPasswordViewModel
+{
+    [Required]
+    public string Token { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Nueva Contraseña")]
+    [StringLength(100, MinimumLength = 8)]
+    public string NewPassword { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare(nameof(NewPassword), ErrorMessage = "Las  contraseñas no coinciden.")]
+    [Display(Name = "Confirmar contraseña")]
+    public string ConfirmPassword { get; set; } = null!;
+}
